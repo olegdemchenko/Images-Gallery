765054ae5e21fba788802bce50982f38b79d1c15
olegdemchenko <olegdemchenko1993@gmail.com>
2020-10-22 11:22:08 +0300

    change download name of image

--- src/view/image.js
+++ src/view/image.js
@@ -2,7 +2,7 @@ export default ({ imagesContainer, imageCardTemplate }) => {
   const renderImages = async (images) => {
     /* eslint-disable no-param-reassign */
     images.forEach(({
-      id, title, description, dataUrl, type,
+      id, title, description, dataUrl, name,
     }) => {
       const newCard = imageCardTemplate.content.cloneNode(true);
       const cardImg = newCard.querySelector('.card-img-top');
@@ -12,9 +12,8 @@ export default ({ imagesContainer, imageCardTemplate }) => {
       const cardText = newCard.querySelector('.card-text');
       cardText.textContent = description;
       const downloadLink = newCard.querySelector('.download-link');
-      const [, imageExtension] = type.split('/');
       downloadLink.setAttribute('href', dataUrl);
-      downloadLink.setAttribute('download', `${title}.${imageExtension}`);
+      downloadLink.setAttribute('download', name);
       const changeButton = newCard.querySelector('.change-button');
       changeButton.setAttribute('data-action', 'change');
       changeButton.setAttribute('data-id', id);
